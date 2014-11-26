19b83ee05e35c424106a4cb4653255583c865aa0
Juan Obando <juanchopx2@gmail.com>
2014-11-25 22:34:51 -0500

    Keep working on initial layout.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,10 @@
 (function(w, d) {
   'use strict';
+
+  var template = document.querySelector('template[is="auto-binding"]');
+  template.pages = [
+    {name: 'Single', hash: 'one'},
+    {name: 'page', hash: 'two'},
+    {name: 'app', hash: 'three'},
+  ];
 }(window, document));
