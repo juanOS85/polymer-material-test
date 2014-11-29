db926308045f0a59596e20f33fce1ac36afedb6d
Juan Obando <juanchopx2@gmail.com>
2014-11-29 14:35:22 -0500

    API JSON files added.  Moved JavaScript to created element.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,1 @@
-(function(w, d) {
-  'use strict';
 
-  // var template = document.querySelector('template[is="auto-binding"]');
-  // template.pages = [
-  //   {name: 'Single', hash: 'one'},
-  //   {name: 'page', hash: 'two'},
-  //   {name: 'app', hash: 'three'},
-  // ];
-})(window, document);
